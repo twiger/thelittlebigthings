f6e1880412fb10f4cf67da59bdac348e1c56c80d
twiger <promisemyself1215@gmai.com>
2016-10-08 22:04:19 +0800

    加入有新文章的提示 & 調整 story_list

--- js/global.js
+++ js/global.js
@@ -61,3 +61,14 @@ function getURLParameter(name) {
 function showStillWorking(selector) {
     $(selector).modal('show');
 }
+
+
+function arraysEqual(arr1, arr2) {
+    if(arr1.length !== arr2.length)
+        return false;
+    for(var i = arr1.length; i--;) {
+        if(arr1[i] !== arr2[i])
+            return false;
+    }
+    return true;
+}
